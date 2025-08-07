6eadf11cf39e59d81687199666edc07acff75fcc
Amirhossein Hemmati <amirhemmati15@yahoo.com>
2025-08-07 18:36:39 +0430

    chore: remove extra fonts and add vaziri

---
+++ src/components/common/BackButton.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div
+    class="w-full bg-[#242629] text-white flex justify-between items-center px-4 py-3"
+  >
+    <span> بازگشت </span>
+    <!-- left arrow -->
+    <span class="w-6 h-6 flex justify-center items-center cursor-pointer">
+      <img src="../../assets/icons/left-arrow.svg" alt="left-Arrow" />
+    </span>
+  </div>
+</template>
