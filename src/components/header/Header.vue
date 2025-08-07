7d9ca155fa1df2750a52bd6a1bbc1dff332fc356
Amirhossein Hemmati <amirhemmati15@yahoo.com>
2025-08-07 17:50:33 +0430

    style: add font and set direction rtl

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -1,9 +1,22 @@
 <script setup lang="ts">
-
+import webLogo from "../../assets/images/FilmTarsnakLogo.svg";
 </script>
 
 <template>
-    <div class="w-full px-4 py-3 bg-red-100">
-
+  <div
+    class="w-full bg-[#242629] text-white flex flex-row-reverse justify-between items-center"
+  >
+    <div class="flex justify-center items-center  gap-x-6">
+        <div class="flex justify-center items-center  gap-x-6">
+            <a>دسته بندی‌ها</a>
+            <a>فیلم</a>
+            <a>سریال</a>
+            <a>بازیگران</a>
+        </div>
+        <span>
+          <img :src="webLogo" />
+        </span>
     </div>
+    <div></div>
+  </div>
 </template>
