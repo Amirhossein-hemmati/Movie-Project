b072c1ada35f9070a64b2321e8e5e1e38b1a319b
Amirhossein Hemmati <amirhemmati15@yahoo.com>
2025-08-08 12:49:43 +0430

    fix: fix subtitle condition in videoPlayArea component

--- src/layout/DefaultLayout.vue
+++ src/layout/DefaultLayout.vue
@@ -4,7 +4,7 @@ import Header from "../components/header/Header.vue";
 </script>
 
 <template>
-  <div class="max-w-[2100px] flex justify-center items-center flex-col">
+  <div class="max-w-[2100px] w-[2100px] flex justify-center items-center flex-col">
     <Header />
     <main class="!px-30 w-full h-full">
       <RouterView />
